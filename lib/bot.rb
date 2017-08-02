1234ec7c7f4fc171984ee3f2b013c81aa11a0f85
Andrew Jackson <andrew@disvelop.net>
2017-08-03 08:44:58 +1000

    basic bot connection working

--- lib/bot.rb
+++ lib/bot.rb
@@ -6,5 +6,21 @@ require 'json'
 require 'yaml'
 
 module Bot
+  require_relative 'bot/configuration.rb'
 
+  Configuration.init
+
+  bot = Discordrb::Commands::CommandBot.new token: Configuration.data['discord_token'],
+                                            client_id: Configuration.data['discord_application_id'],
+                                            prefix: Configuration.data['command_prefix']
+
+  Dir['./lib/plugins/*.rb'].each do |plugin|
+    require plugin
+    bot.include! plugin
+    plugin.init(bot)
+  end
+
+  bot.run :async
+  gets
+  bot.stop
 end
